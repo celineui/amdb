3956d4906eb19b7fcffe07b8f7cdc62db7a6d3c3
Raghu Betina <rbetina@gmail.com>
2013-02-06 11:02:57 -0600

    Built association methods for movies and roles. Fixed up views. Still need to write association methods for actors.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,17 @@
 class Role < ActiveRecord::Base
   attr_accessible :actor_id, :character_name, :movie_id
+
+  def movie
+    return Movie.find_by_id(self.movie_id)
+  end
+
+  def actor
+    return Actor.find_by_id(self.actor_id)
+  end
 end
+
+
+
+
+
+
