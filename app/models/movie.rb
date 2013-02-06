115a434100886c56ef192514ce2a4fac931bec5a
Raghu Betina <rbetina@gmail.com>
2013-02-06 09:53:25 -0600

    Wrote the Movie#director and Director#movies methods.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :director_id, :title, :year
+  attr_accessible :title, :year, :director_id
+
+  def director
+    return Director.find_by_id(self.director_id)
+  end
 end
