d3e9f1a3ab2f14ea21249686ba11afedeb24d315
Raghu Betina <rbetina@gmail.com>
2013-02-08 08:52:02 -0600

    Added back association methods. About to write validations.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,12 @@ class Movie < ActiveRecord::Base
   attr_accessible :title, :year, :director_id
 
   # belongs to director
+  def director
+    return Director.find_by_id(self.director_id)
+  end
 
   # has many roles
+  def roles
+    return Role.where(:movie_id => self.id)
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -2,8 +2,14 @@ class Role < ActiveRecord::Base
   attr_accessible :actor_id, :character_name, :movie_id
 
   # belongs to movie
+  def movie
+    return Movie.find_by_id(self.movie_id)
+  end
 
   # belongs to actor
+  def actor
+    return Actor.find_by_id(self.actor_id)
+  end
 end
 
 
